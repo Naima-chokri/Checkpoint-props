7d0ca378715f13ed59ae95bd2b6c7f4d82a40eda
Naima-chokri <nematchokri@gmail.com>
2022-05-15 12:18:12 +0100

    fixing props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Profile from "./Profile/Profile";
 
 function App() {
-  const FirstName='Naima'
-  const LastName='Chokri'
-  const bio ="i'm a web developer i like "
+  const FirstName='Naima';
+  const LastName='Chokri';
+  const profession='Web Developer'
+  const adresse= 'Happy city-Gafsa Tunis (+216) 94-902-044 '
+  const bio = "I’m a software engineer specializing in building (and occasionally designing) Currently, I’m focused on building web and Mobile Applications."
+  const email ='naima-chokri@outlook.fr';
+  const handleName =()=> alert(FirstName)
   return (
-    <Profile FirstName={FirstName} LastName={LastName} bio = {bio} />
+    <Profile FirstName={FirstName} LastName={LastName} profession= {profession} adresse = {adresse} bio = {bio} email={email} handleName={handleName}>
+    <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.png" alt="..." />
+    </Profile>
   );
 }
 
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const Profile = ({FirstName,LastName, bio}) => {
+const Profile = ({FirstName,LastName, profession, adresse, bio,  email ,children, handleName}) => {
   return (
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
         <a className="navbar-brand js-scroll-trigger" href="#page-top">
             <span className="d-block d-lg-none">Clarence Taylor</span>
-            <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.png" alt="..." /></span>
+            <span className="d-none d-lg-block">{children}</span>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav">
-            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
+            <li className="nav-item" onClick={() => handleName(FirstName)}><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
             <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
             <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#education">Education</a></li>
             <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
@@ -30,12 +30,10 @@ const Profile = ({FirstName,LastName, bio}) => {
                     <span className="text-primary">{LastName}</span>
                 </h1>
                 <div className="subheading mb-5">
-                    3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
-                    <a href="mailto:name@email.com">name@email.com</a>
+                  I'm a  {profession} based in {adresse}
+                    <a href="mailto:naima-chori@outlook.fr">{email}</a>
                 </div>
-                <p className="lead mb-5">  I’m a software engineer specializing in building (and
-                occasionally designing) Currently, I’m focused on building web
-                and Mobile Applications.</p>
+                <p className="lead mb-5">{bio}</p>
                 <div className="social-icons">
                     <a className="social-icon" href="https://www.linkedin.com/in/naima-chokri-57208b11b/"><i className="fab fa-linkedin-in" /></a>
                     <a className="social-icon" href="https://github.com/Naima-chokri"><i className="fab fa-github" /></a>
